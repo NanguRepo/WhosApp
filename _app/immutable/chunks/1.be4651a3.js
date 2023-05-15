0d49d45a79b0d8d00d4dbf6c6391a6a84f4a86dd
NanguRepo <nanguthenangu@gmail.com>
2023-05-15 09:50:08 +0200

    Updates

--- _app/immutable/chunks/1.be4651a3.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.b4ab5c1a.js";export{t as component};
---
+++ _app/immutable/chunks/2.96c0aa1d.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/_page.svelte.8980a2ba.js";export{t as component};
